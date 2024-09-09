81b7d307501323cf80fb3dc89410233c96743d47
Lyndon Panton <LyndonPanton@gmail.com>
2024-09-09 15:30:32 +0100

    Aligned header content

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Document has been fully loaded.");
+
+    // Get the current date (and time?)
+
+    // Get the current weather
 });
