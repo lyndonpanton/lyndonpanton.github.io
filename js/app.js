971ed7ef9090c559ba620dc095ba23deca811758
Lyndon Panton <LyndonPanton@gmail.com>
2024-09-10 16:25:03 +0100

    Dropdown and dropdown icon can now be toggled

--- js/app.js
+++ js/app.js
@@ -1,7 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Document has been fully loaded.");
 
+    // Toggle dropdown icon
+    const moreDropdown = document.getElementById("nav-top-more")
+    const moreListItem = document.getElementsByClassName("nav-item-end")[0];
+    const moreDropdownIcon =
+        moreListItem.getElementsByClassName("fa-angle-down")[0];
+    moreListItem.addEventListener("click", toggleMoreDropdown);
+
     // Get the current date (and time?)
 
     // Get the current weather
+
+    // Change to toggle using rotation instead of an immediate change
+    // Edit the fontawesome CSS classes and use transitions to achieve this
+    function toggleMoreDropdown() {
+        moreDropdown.classList.toggle("hidden");
+        moreDropdownIcon.classList.toggle("fa-angle-down");
+        moreDropdownIcon.classList.toggle("fa-angle-up");
+    }
 });
