2cce55c9b51578822d6eabc90e42869ba5f0e0c2
Lyndon Panton <LyndonPanton@gmail.com>
2024-09-16 17:15:28 +0100

    Tab styling can be toggled on click

--- js/app.js
+++ js/app.js
@@ -13,11 +13,55 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
+    const videosTabs = document.getElementsByClassName("videos-tab-link");
+    for (let i = 0; i < videosTabs.length; i++) {
+        videosTabs[i].addEventListener("click", function() {
+            changeVideosTab(this);
+        });
+    }
+
+    let invisible = document.getElementsByClassName("invisible");
+
+    for (let i = 0; i < invisible.length; i++) {
+        invisible[i].style.display = "none";
+        // invisible[i].classList.add("invisible");
+    }
+
     // Get the current date and time
 
     // Get the current weather
 
     // Use an API to get random articles
+
+    function changeVideosTab(tab) {
+        /*
+            #videos-content
+                #videos-tabs
+                    .videos-tab-link
+                #videos-list
+                    #videos-list-latest
+                        article.article-video
+                        // ...
+                    #videos-list-elections
+                    #videos-list-conflicts
+                    #videos-list-explainers
+                    #videos-list-interviews
+         */
+
+        for (let i = 0; i < videosTabs.length; i++) {
+            if (videosTabs[i] === tab) {
+                videosTabs[i].classList.add("videos-tab-selected");
+            } else {
+                videosTabs[i].classList.remove("videos-tab-selected");
+            }
+        }
+
+        // Get the index of the tab pressed in its parent (#videos-tabs)
+        // Get the next sibling of the parent (#videos-list)
+        // Loop through all children of #videos-list
+        // Use the index to make the relevant section visible
+        // Make the other sections invisible
+    }
 
     function loadMoreArticles(button) {
         let nextArticles = button.parentElement.nextElementSibling;
@@ -65,5 +109,6 @@ document.addEventListener("DOMContentLoaded", function() {
     - Bird
     - Tortoise
     - Brown bear
-    - Komodo dragon
+    - Giraffe
+    - Red panda
  */
